8561c088b683100a68e083d5a789ab169d9de4a7
Luis Nunez <61037170+guitarpro777@users.noreply.github.com>
2020-12-26 00:09:10 -0700

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+//1. Create Entry Component
+//2. Create props to replace hardcoded data.
+//3a. Import the emojipedia const.
+//3b. Map through the emojipedia array and render Entry components.
+
+//{
+//id: 1,
+//emoji: "💪",
+//name: "Tense Biceps",
+//meaning:
+//"“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+//},
+
+function createEntry(emojiTerm) {
+  return (
+    <Entry
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      description={emojiTerm.meaning}
+    />
+  );
+}
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+    </div>
+  );
+}
+
+export default App;
